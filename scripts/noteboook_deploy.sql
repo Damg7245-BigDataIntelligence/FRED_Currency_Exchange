368ad30aa1a16e207ebd998003e33d95f830a196
Sahil Mutha <sahilmutha1999@gmail.com>
2025-02-28 15:58:21 -0500

    Formating final jinja

--- scripts/noteboook_deploy.sql
+++ scripts/noteboook_deploy.sql
@@ -1,31 +1,25 @@
 --!jinja
  
--- Create schemas based on environment
-CREATE SCHEMA IF NOT EXISTS "FRED_DB"."{{env}}_RAW_SCHEMA";
-CREATE SCHEMA IF NOT EXISTS "FRED_DB"."{{env}}_HARMONIZED_SCHEMA";
-CREATE SCHEMA IF NOT EXISTS "FRED_DB"."{{env}}_ANALYTICS_SCHEMA";
- 
--- Deploy load_raw_data notebook to INTEGRATIONS schema
-CREATE OR REPLACE NOTEBOOK IDENTIFIER('"FRED_DB"."INTEGRATIONS"."{{env}}_load_raw_data"')
+CREATE OR REPLACE NOTEBOOK IDENTIFIER('"FRED_DB"."{{env}}_{{schema1}}"."{{env}}_load_raw_data"')
     FROM '@"FRED_DB"."INTEGRATIONS"."FRED_GIT_REPO"/branches/"{{branch}}"/steps/load_raw_data/'
     QUERY_WAREHOUSE = 'FRED_WH'
     MAIN_FILE = 'load_raw_data.ipynb';
  
-ALTER NOTEBOOK "FRED_DB"."INTEGRATIONS"."{{env}}_load_raw_data" ADD LIVE VERSION FROM LAST;
+ALTER NOTEBOOK "FRED_DB"."{{env}}_{{schema1}}"."{{env}}_load_raw_data" ADD LIVE VERSION FROM LAST;
  
 -- Deploy harmonize_data notebook to INTEGRATIONS schema
-CREATE OR REPLACE NOTEBOOK IDENTIFIER('"FRED_DB"."INTEGRATIONS"."{{env}}_harmonize_data"')
+CREATE OR REPLACE NOTEBOOK IDENTIFIER('"FRED_DB"."{{env}}_{{schema2}}"."{{env}}_harmonize_data"')
     FROM '@"FRED_DB"."INTEGRATIONS"."FRED_GIT_REPO"/branches/"{{branch}}"/steps/harmonized_data/'
     QUERY_WAREHOUSE = 'FRED_WH'
     MAIN_FILE = 'harmonize_data.ipynb';
  
-ALTER NOTEBOOK "FRED_DB"."INTEGRATIONS"."{{env}}_harmonize_data" ADD LIVE VERSION FROM LAST;
+ALTER NOTEBOOK "FRED_DB"."{{env}}_{{schema2}}"."{{env}}_harmonize_data" ADD LIVE VERSION FROM LAST;
  
 -- Deploy analytics notebook to INTEGRATIONS schema
-CREATE OR REPLACE NOTEBOOK IDENTIFIER('"FRED_DB"."INTEGRATIONS"."{{env}}_analytics"')
+CREATE OR REPLACE NOTEBOOK IDENTIFIER('"FRED_DB"."{{env}}_{{schema3}}"."{{env}}_analytics"')
     FROM '@"FRED_DB"."INTEGRATIONS"."FRED_GIT_REPO"/branches/"{{branch}}"/steps/analytics/'
     QUERY_WAREHOUSE = 'FRED_WH'
     MAIN_FILE = 'analytics.ipynb';
  
-ALTER NOTEBOOK "FRED_DB"."INTEGRATIONS"."{{env}}_analytics" ADD LIVE VERSION FROM LAST;
+ALTER NOTEBOOK "FRED_DB"."{{env}}_{{schema3}}"."{{env}}_analytics" ADD LIVE VERSION FROM LAST;
  
